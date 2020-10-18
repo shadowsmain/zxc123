5e2571fc5eebc815b4c1e9a7da1c6eed7a1629d7
vldln-biven'мамы.net <strifee322@gmail.com>
2020-10-18 19:18:29 +0500

    hw

---
+++ zxc/mainapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+
+def aboutme(request):
+    return render(request, 'mainapp/aboutme.html')
+
+
+def last(request):
+    return render(request, 'mainapp/last.html')

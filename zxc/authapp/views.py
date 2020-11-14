232d4ecb8ff5b64a0196b90683f6a4e626162155
vldln-biven'мамы.net <strifee322@gmail.com>
2020-11-14 10:09:11 +0500

    basketapp

--- zxc/authapp/views.py
+++ zxc/authapp/views.py
@@ -1,15 +1,42 @@
-from django.contrib.auth.forms import AuthenticationForm
+import django.contrib.auth as auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from authapp.forms import LoginForm
+from authapp.forms import LoginForm, RegisterForm
 
 
 def login(request):
-    # form = AuthenticationForm()
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            auth.login(request, form.get_user())
+            return HttpResponseRedirect('/')
+    else:
+        form = LoginForm()
 
     context = {
         'page_title': 'авторизация',
         'form': form,
     }
     return render(request, 'authapp/login.html', context)
+
+
+def logout(reqest):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = RegisterForm
+
+    context = {
+        'page_title': 'Регистрация',
+        'form': form,
+    }
+    return render(request, 'authapp/register.html', context)

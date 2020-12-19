1f74229a0cfac9d275ef519a8492efd2e4f80092
vldln-biven'мамы.net <strifee322@gmail.com>
2020-12-19 09:29:41 +0500

    bookapp

--- zxc/neoclassicaapp/admin.py
+++ zxc/neoclassicaapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Neoclassica
+from neoclassicaapp.models import Neoclassica
 
 admin.site.register(Neoclassica)
--- zxc/neoclassicaapp/views.py
+++ zxc/neoclassicaapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from neoclassicaapp.models import Neoclassica
+
+
+def neoclassica(request):
+    neoclassics = Neoclassica.objects.all()
+    context = {
+        'neoclassics': neoclassics,
+        'page_title': 'неоклассика',
+    }
+
+    return render(request, 'neoclassicaapp/neoclassica.html', context)
